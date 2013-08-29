3b8cfd7a77e0ee15e3ee3571bf47482b2d38862b
Hlib_Mykhailenko <tas.nix@gmail.com>
2013-08-29 19:23:28 +0300

    sadf

--- WebContent/script2.js
+++ WebContent/script2.js
@@ -8,26 +8,30 @@ $(document).ready(function() {
 });
 
 function loginLostFocus(input) {
-	var validated = validate(loginPattern, input);
-	if(validated['invalid']){
-		alert(validated['validationMessage']);
-	}
-}
-function part(func, obj){
-	return function(){
-		
-	}
+	var div = document.getElementById('loginPId');
+	div.removeAttribute('class');
+	validateWrap(loginPattern, input, div);
 }
 function passwordLostFocus(input) {
-
+	var div = document.getElementById('passwordPId');
+	div.removeAttribute('class');
+	validateWrap(passwordPattern, input, div);
 }
 
+function validateWrap(pattern, input, div){
+	var validated = validate(pattern, input);
+	if(validated['invalid']){
+		alert(validated['validationMessage']);
+		div.setAttribute("class", "invalidP");
+	}
+	
+}
 function validate(pattern, input){
-	if(input.length == 0){
+	if(input.value.length == 0){
 		return { invalid : true, 
 				 valid : false, 
 				 validationMessage : "This field is required and cannot be empty"};
-	}else if(!pattern.text(input.value)){
+	}else if(!pattern.test(input.value)){
 		return { invalid : true, 
 			valid : false, 
 			validationMessage : "This field must have at least one letter and one number"};
